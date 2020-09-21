5c8a5bc90ae11c15aaf81c5206fc102304e7dc44
Luke Williams <luke@iquidus.io>
2020-09-22 01:59:56 +0200

    test4

--- src/mappings/oracle.ts
+++ src/mappings/oracle.ts
@@ -1,22 +1,21 @@
 /* eslint-disable prefer-const */
 import { Oracle } from '../types/schema'
-import { EthereumBlock } from '@graphprotocol/graph-ts'
-import { BigDecimal, BigInt } from '@graphprotocol/graph-ts/index'
+import { EthereumBlock, json, JSONValueKind } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt, Boolean } from '@graphprotocol/graph-ts/index'
 import { exponentToBigDecimal } from './helpers'
 import { ZERO_BD } from './helpers'
 
-const ORACLE_ADDRESS = '0x96db5d68b8d40c4c3d1d5d3e366d40152c1e23a2' // created block 1321197
-
+const XORACLE_ADDRESS = '0xaeff2f7644f1c615adb309513c4cb564f44bb68f' // created block 1322544
 
 // dummy for testing
 export function getEthPriceInUSD(): BigDecimal {
   // fetch price from market oracle
-  let oracle = Oracle.load(ORACLE_ADDRESS)
+  let oracle = Oracle.load(XORACLE_ADDRESS)
 
   if (oracle !== null) {
-    let data: BigInt[] = oracle.getData
-    if (data !== null && data[0] !== null) {
-      let usd = data[0].toBigDecimal()
+    let data: BigInt = oracle.getPrice
+    if (data !== null) {
+      let usd = data.toBigDecimal()
       if (usd !== null) {
         usd = usd.div(exponentToBigDecimal(BigInt.fromI32(6)))
         return usd
