9360cf426ce3af60d4679abb63510eddea01e2ab
Luke Williams <luke@iquidus.io>
2020-09-23 21:45:07 +0200

    x

--- src/mappings/oracle.ts
+++ src/mappings/oracle.ts
@@ -12,23 +12,30 @@ const XORACLE_ADDRESS = '0xaefF2F7644f1C615aDb309513c4CB564F44Bb68F' // created 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch price from market oracle
   let oracle = PriceStore.load(XORACLE_ADDRESS)
-  if (oracle !== null) {
-    return oracle.usd
-  } else {
+  if (oracle == null) {
     return BigDecimal.fromString('0.2') // ZERO_BD
+  } else {
+    return oracle.usd
   }
 }
 
 export function priceUpdate(block: EthereumBlock): void {
-  let contract = Oracle.bind(Address.fromString(XORACLE_ADDRESS))
   let oracle = PriceStore.load(XORACLE_ADDRESS)
   if (oracle == null) {
     oracle = new PriceStore(XORACLE_ADDRESS)
-    oracle.usd = ZERO_BD
-    oracle.save()
+    oracle.usd =  BigDecimal.fromString('0.2') // ZERO_BD
   } else {
-    let data: BigInt = contract.getPriceUsd()
-    oracle.usd = data.toBigDecimal().div(exponentToBigDecimal(BigInt.fromI32(6)))
-    oracle.save()
+    let contract = Oracle.bind(Address.fromString(XORACLE_ADDRESS))
+    if (contract == null) {
+      oracle.usd =  BigDecimal.fromString('0.2') // ZERO_BD
+    } else {
+      let data: BigInt = contract.getPriceUsd()
+      if (data == null) {
+        oracle.usd =  BigDecimal.fromString('0.2') // ZERO_BD
+      } else {
+        oracle.usd = data.toBigDecimal().div(exponentToBigDecimal(BigInt.fromI32(6)))
+      }
+    }
   }
+  oracle.save()
 }
